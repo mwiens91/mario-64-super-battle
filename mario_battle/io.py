a80a9365e53e8ba556fc452864082b4bc3e62dcc
Matt Wiens <mwiens91@gmail.com>
2018-08-19 14:51:21 -0700

    Add stuff for the beginning of the battle

--- mario_battle/io.py
+++ mario_battle/io.py
@@ -2,3 +2,27 @@
 
 All using the command line, at present.
 """
+
+
+def display_welcome_message():
+    """Displays a welcome message to the user."""
+    print("HEY WELCOME TO MARIO")
+
+def get_player_names():
+    """Gets the player names from the user.
+
+    Returns:
+        A tuple of two strings, containing the player names.
+    """
+    return ("Matt", "Branko")
+
+def get_number_of_rounds():
+    """Gets the number of rounds from the user.
+
+    The number of rounds must be odd and be no more than 15 (the number
+    of courses in Mario 64).
+
+    Returns:
+        An integer specifying the number of rounds.
+    """
+    return 5
--- mario_battle/main.py
+++ mario_battle/main.py
@@ -1,8 +1,20 @@
 """Contains the main function for battling."""
 
 from mario_battle.battle_class import MarioBattle
+from mario_battle.io import (
+    display_welcome_message,
+    get_number_of_rounds,
+    get_player_names,
+)
 
 
 def main():
     """The main function."""
-    pass
+    # Display a welcome message
+    display_welcome_message()
+
+    # Get player names
+    player1, player2 = get_player_names()
+
+    # Get number of rounds
+    rounds = get_number_of_rounds()
