fa2781fd025ca2e3e904f9cd019d391e1da970e2
Matt Wiens <mwiens91@gmail.com>
2018-08-19 15:00:59 -0700

    Add list of Mario 64 courses
    Deciding to include Bowser courses in here, even though they're not
    technically a proper 6-star course. User can exclude them if they want.

--- mario_battle/constants.py
+++ mario_battle/constants.py
@@ -1,3 +1,22 @@
 """Contains constants for the program."""
 
-COURSE_DICTIONARY = {}
+COURSE_DICTIONARY = {
+    1: "Bob-omb Battlefield",
+    2: "Whomp's Fortress",
+    3: "Jolly Roger Bay",
+    4: "Cool, Cool Mountain",
+    5: "Big Boo's Haunt",
+    6: "Hazy Maze Cave",
+    7: "Lethal Lava Land",
+    8: "Shifting Sand Land",
+    9: "Dire, Dire Docks",
+    10: "Snowman's Land",
+    11: "Wet-Dry World",
+    12: "Tall, Tall Mountain",
+    13: "Tiny, Huge Island",
+    14: "Tick Tock Clock",
+    15: "Rainbow Ride",
+    16: "Bowser in the Dark World",
+    17: "Bowser in the Fire Sea",
+    18: "Bowser in the Sky",
+}
