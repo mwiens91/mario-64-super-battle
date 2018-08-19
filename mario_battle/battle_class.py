1c106b6e30a7d7f3524470d11c2e78539cb8da79
Branko Bajcetic <bajcetic.branko@gmail.com>
2018-08-19 16:42:32 -0700

    Add show times function to Mario Battle class

--- mario_battle/battle_class.py
+++ mario_battle/battle_class.py
@@ -1,25 +1,29 @@
 """Contains an object to keep track of battle results."""
 import datetime
 
+def show_times(seconds):
+    """returns time elapsed in "hours:minutes:seconds.millisecond" string"""
+    hours = seconds // 3600
+    minutes = (seconds - (hours*3600)) // 60
+    seconds = seconds - (hours*3600) - (minutes*60)
+    time_elapsed = "{:02.0f}:{:02.0f}:{:06.3f}".format(hours,minutes,seconds)
+    return time_elapsed
+
 class MarioBattle:
-    """initialize player names, number of rounds, and the courses chosen to play"""
     def __init__(self, player1, player2, num_rounds, courses):
+        """initialize player names, number of rounds, and the courses chosen to play"""
         self.courses = courses
         self.player1 = player1
         self.player2 = player2
         self.num_rounds = num_rounds
-        self.p1_total_time = datetime.time()
-        self.p2_total_time = datetime.time()
-        self.p1_times = []
-        self.p2_times = []
+        self.p1_total_time = 0
+        self.p2_total_time = 0
+        self.p1_times = [] #list of course times
+        self.p2_times = [] #list of course times
     
-    """p1_time and p2_time are datetime.time objects"""
     def add_times(self, p1_time, p2_time):
-        temp1 = self.p1_total_time
-        temp2 = self.p1_total_time
-        self.p1_total_time.replace(temp1.hour+p1_time.hour, temp1.minute+p1_time.minute, 
-                                   temp1.second+p1_time.second)
-        self.p2_total_time.replace(temp2.hour+p2_time.hour, temp2.minute+p2_time.minute, 
-                                   temp2.second+p2_time.second)
+        """p1_time and p2_time are ints representing total seconds"""
+        self.p1_total_time += p1_time
+        self.p2_total_time += p2_time
         self.p1_times.append(p1_time)
         self.p2_times.append(p2_time)
