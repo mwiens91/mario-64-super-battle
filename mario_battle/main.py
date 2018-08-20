7c7c7f7d18b95d5be6165c17e8b4a39813b4d379
Matt Wiens <mwiens91@gmail.com>
2018-08-19 17:17:36 -0700

    Add logic for main function battles

--- mario_battle/main.py
+++ mario_battle/main.py
@@ -22,3 +22,49 @@ def main():
 
     # Select which stages to use
     courses = get_courses(rounds)
+
+    # Initialize the battle
+    battle = MarioBattle(
+        player1=player1,
+        player2=player2,
+        num_rounds=rounds,
+        courses=courses)
+
+    # Battle!
+    for round_ in range(1, rounds + 1):
+        # Determine starting player
+        first_player, second_player = battle.get_players(round_)
+
+        # Determine stage
+        if round_ == rounds:
+            course = get_course(
+                courses=battle.courses,
+                player=first_player,
+                last_stage=True)
+        else:
+            course = get_course(
+                courses=battle.courses,
+                player=first_player,
+                last_stage=True)
+
+        # Player 1's turn!
+        first_player_time = time_player(
+            player=first_player,
+            course=course)
+
+        # Player 2's turn!
+        second_player_time = time_player(
+            player=second_player,
+            course=course)
+
+        # Return results
+        post_dict = {
+            'round': round_,
+            'course': course,
+            'times': {
+                first_player: first_player_time,
+                second_player: second_player_time
+            }
+        }
+
+        battle.post_results(post_dict)
